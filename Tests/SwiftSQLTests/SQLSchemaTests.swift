762df243c77280ba58a6c7fd0d1f71c1408a8774
Jason Jobe <box2019@jasonjobe.com>
2023-02-03 01:15:37 -0500

    added tests for strict and loose instantiation

--- Tests/SwiftSQLTests/SQLSchemaTests.swift
+++ Tests/SwiftSQLTests/SQLSchemaTests.swift
@@ -24,7 +24,7 @@ final class SQLSchemaTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testInstantiate() throws {
+    func sampleDatabase() throws -> SQLConnection {
         let db = try! SQLConnection(location: .memory())
         try db.execute("CREATE TABLE Test (name TEXT, ndx INT)")
         
@@ -32,9 +32,57 @@ final class SQLSchemaTests: XCTestCase {
         try db.prepare("INSERT INTO Test (name, ndx) VALUES (?, ?)")
             .bind("alpha", 1)
             .execute()
-        
-        // WHEN/THEN reads the value
-        let statement = try db.prepare("SELECT name, ndx FROM Test")
+        return db
+    }
+    
+    func testInstantiateStrict() throws {
+        // We delibertly do NOT select all columns
+        // So it should raise an exception
+        let db = try sampleDatabase()
+        let statement = try db.prepare("SELECT name FROM Test")
+        XCTAssertTrue(try statement.step())
+        struct S: ExpressibleByDefault {
+            var name: String
+            var ndx: Int
+            
+            init(defaultContext: ()) {
+                name = ""
+                ndx = 0
+            }
+        }
+        let s = Schema(for: S.self)
+        do {
+            let v: S = try s.instantiate(from: statement, strict: true)
+            print (v)
+        } catch {
+            print(error.localizedDescription)
+        }
+//        assertSnapshot(matching: v, as: .dump)
+    }
+
+    func testInstantiateLoose() throws {
+        // We delibertly do NOT select all columns
+        // So some default values should remain
+        let db = try sampleDatabase()
+        let statement = try db.prepare("SELECT name FROM Test")
+        XCTAssertTrue(try statement.step())
+        struct S: ExpressibleByDefault {
+            var name: String
+            var ndx: Int
+            
+            init(defaultContext: ()) {
+                name = ""
+                ndx = 0
+            }
+        }
+        let s = Schema(for: S.self)
+        let v: S = try s.instantiate(from: statement, strict: false)
+        assertSnapshot(matching: v, as: .dump)
+    }
+    
+    func testInstantiate() throws {
+        let db = try sampleDatabase()
+         let statement = try db.prepare("SELECT name, ndx FROM Test")
         XCTAssertTrue(try statement.step())
         struct S: ExpressibleByDefault {
             var name: String
@@ -47,23 +95,20 @@ final class SQLSchemaTests: XCTestCase {
         }
         let s = Schema(for: S.self)
         let v: S = try s.instantiate(from: statement)
-        print(v)
+        assertSnapshot(matching: v, as: .dump)
     }
 
-    func testMD() throws {
-        let md = swift_metadata(of: Schema.self)
-        print(md)
-        let t = md.type == Int.self
-        print(t)
-        for p in md.properties {
-            print(p.name, p.metadata.kind == .optional)
-        }
-     }
+//    func testMD() throws {
+//        let md = swift_metadata(of: Schema.self)
+//        print(md)
+//        let t = md.type == Int.self
+//        print(t)
+//        for p in md.properties {
+//            print(p.name, p.metadata.kind == .optional)
+//        }
+//     }
     
     func testSchema() throws {
-//        let p: Person = .defaultValue()
-//        print(p)
-        
         let s = Schema(for: Person.self)
         assertSnapshot(matching: s.sql(create: "person"), as: .lines)
         assertSnapshot(matching: s.sql(insert: "person"), as: .lines)
